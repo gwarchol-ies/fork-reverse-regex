eb32c64f7141ccbe2b9a5008a55180fd7d687930
Daniel Hoffmann <daniel.hoffmann@jtl-software.com>
2021-01-18 13:40:40 +0100

    fixed tests and add PHP 8 compatibility

--- src/ReverseRegex/Test/Basic.php
+++ src/ReverseRegex/Test/Basic.php
@@ -2,17 +2,19 @@
 namespace ReverseRegex\Test;
 
 use PHPUnit\Framework\TestCase;
-use ReverseRegex\PimpleBootstrap;
-use Pimple\Pimple;
+// use ReverseRegex\PimpleBootstrap;
+// use Pimple\Pimple;
 
 abstract class Basic extends TestCase
 {
     public function createApplication()
     {
-        $boot = new PimpleBootstrap(); 
-        $pimple = $boot->boot(new Pimple());  
+        /*
+        $boot = new PimpleBootstrap();
+        $pimple = $boot->boot(new Pimple());
         return $pimple;
+        */
     }
-    
+
 }
 /* End of File */
